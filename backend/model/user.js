1af04db3b3d8f7c3af3853aa3266797caa56898e
HabenMebrahtom <d_haben10@yahoo.com>
2022-05-29 17:10:50 +0200

    creating a user schema for the user

--- backend/model/user.js
+++ backend/model/user.js
@@ -3,4 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true}
-})
+},
+    {collection: 'users'})
+
+
+const userModal = mongoose.model('userSchema', userSchema);
+module.exports = userModal;
