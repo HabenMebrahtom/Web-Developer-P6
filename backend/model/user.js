e5f09940a09de3f07849091395b06f707777555f
HabenMebrahtom <d_haben10@yahoo.com>
2022-05-29 17:11:42 +0200

    creating a contollers folder with sauce and user files

--- backend/model/user.js
+++ backend/model/user.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true}
 },
-    {collection: 'users'})
+    { collection: 'users' }
+)
 
 
 const userModal = mongoose.model('userSchema', userSchema);
