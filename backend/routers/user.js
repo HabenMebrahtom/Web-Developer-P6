3ef2f4ac942f986e50e4edc1818a64d8c611deec
HabenMebrahtom <d_haben10@yahoo.com>
2022-06-08 23:51:59 +0200

    deleting the auth import to the file

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const userRouter = express.Router();
-const auth = require('../middleware/auth')
 
 
 const userControllers = require('../controllers/user');
 
-
-userRouter.get('/profile', userControllers.getUserProfile)
 userRouter.post('/signup', userControllers.registerUser);
-userRouter.post('/login', auth, userControllers.loginUser);
+userRouter.post('/login', userControllers.loginUser);
 
 module.exports = userRouter;
