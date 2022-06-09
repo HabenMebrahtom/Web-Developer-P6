1a3086cbbf506a50292337cb6226d67a9be7c5b4
HabenMebrahtom <d_haben10@yahoo.com>
2022-06-09 15:12:25 +0200

    creating a schema according the instruction

--- backend/model/sauce.js
+++ backend/model/sauce.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
+const nanoid = require('nanoid')
 
 const sauceSchema = new mongoose.Schema({
-    sauce: {type: String},
-    imageUrl: {data: Buffer, type: String}
+  userId: {
+    type: String,
+    required: true,
+    default: () => nanoid(7),
+    index: {unique: true}
+    },
+  name: { type: String },
+  manufacturer: { type: String },
+  description: { type: String },
+  mainPepper: { type: String },
+  imageUrl: { data: Buffer, type: String },
+  heat: { type: Number },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: [{ type: String }],
+  usersDisliked: [{ type: String }]
 });
 
 const sauceModel = mongoose.model('sauceSchema', sauceSchema);
 module.exports = sauceModel;
+
